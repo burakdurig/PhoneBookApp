36fb14d9617783d8ae3c2e5086c455044b1b45f3
Durig, Burak <burak.durig@nissan-usa.com>
2025-01-03 18:07:23 -0500

    PhoneBook Addition

--- app/src/main/java/com/mastercoding/firebaseapp/MainActivity.java
+++ app/src/main/java/com/mastercoding/firebaseapp/MainActivity.java
@@ -9,62 +9,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mastercoding.firebaseapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<User> users;
+    private RecyclerView recyclerView;
+    private MyAdapter userAdapter;
+    private ActivityMainBinding binding;
+
+    // Firebase
+    DatabaseReference databaseReference;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
 
-        // Initialize and Access the Firebase Database.
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Users");
 
-        // Get a reference to a specific node in teh database
-        DatabaseReference myRef = database.getReference("Users");
-        // THe above changed from "messages" => "Users"
+        // RecyclerView with Databinding
+        recyclerView = binding.recyclerView;
 
-        // Write a value to the specific database location.
-        // the below is commented out because it already was upload when
-        // application was already ran.
-//        User user1 = new User("Jack", "jack@gmail.com");
-//        myRef.setValue(user1);
 
-        // Write a value to the specified database location
-        // myRef.setValue("Hello from our Course!");
 
-        TextView textView = findViewById(R.id.textView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // the below is commented out so we can use the "Users" path.
-        // The below will add data from a database snapshot and output the data to a specified value
-        // as specific within the onDataChange in our case "textView" is the output. myRef is just a link to the database reference.
-        myRef.addValueEventListener(new ValueEventListener() {
+        // Fetch the data from Firebase into RecyclerView
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // we don't need the below any more.
-//                String newValue = snapshot.getValue(String.class);
-//                textView.setText(newValue);
-                    User user = snapshot.getValue(User.class);
-                    textView.setText("Email: "+user.getEmail());
-                    // The above will read from teh Realtime Database and will output in the app
-                // link to the firebase:
-                // https://console.firebase.google.com/project/fir-demo-app-3d7e2/database/fir-demo-app-3d7e2-default-rtdb/data?fb_gclid=Cj0KCQiAst67BhCEARIsAKKdWOnaaDcfN9nuvfN053h3oLod99KEugIOGPanYGquGRZC9wUI_EdBxRwaAiJIEALw_wcB
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+                    users.add(user);
 
+                }
+                // notify an adapter associated with a recyclerView
+                // that the underlying dataset has changed.
+                userAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Error handling.
+                // Error Handling
             }
         });
+
+        users = new ArrayList<>();
+
+        userAdapter = new MyAdapter(
+                this,
+                users
+        );
+        recyclerView.setAdapter(userAdapter);
 
     }
 }
